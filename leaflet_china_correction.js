6108d249b6597a8c943721d9a21c81a15d70cc91
Yizhou Chen <geoyee@yeah.net>
2022-09-18 14:48:05 +0800

    Update leaflet_china_correction.js

--- leaflet_china_correction.js
+++ leaflet_china_correction.js
@@ -1,16 +1,19 @@
-L.CRS.Baidu = new L.Proj.CRS('EPSG:900913', '+proj=merc +a=6378206 +b=6356584.314245179 +lat_ts=0.0 +lon_0=0.0 +x_0=0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs', {
-    resolutions: function () {
-        var level = 19
-        var res = [];
-        res[0] = Math.pow(2, 18);
-        for (var i = 1; i < level; i++) {
-            res[i] = Math.pow(2, (18 - i))
-        }
-        return res;
-    }(),
-    origin: [0, 0],
-    bounds: L.bounds([20037508.342789244, 0], [0, 20037508.342789244])
-});
+if (L.Proj) {
+    L.CRS.Baidu = new L.Proj.CRS('EPSG:900913', '+proj=merc +a=6378206 +b=6356584.314245179 +lat_ts=0.0 +lon_0=0.0 +x_0=0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs', {
+        resolutions: function () {
+            var level = 19
+            var res = [];
+            res[0] = Math.pow(2, 18);
+            for (var i = 1; i < level; i++) {
+                res[i] = Math.pow(2, (18 - i))
+            }
+            return res;
+        }(),
+        origin: [0, 0],
+        bounds: L.bounds([20037508.342789244, 0], [0, 20037508.342789244])
+    });
+}
+
 
 L.CoordConver = function () {
     var pi = 3.1415926535897932384626;
