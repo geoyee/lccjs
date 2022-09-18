0b6dd409e8876c5a9b8eef7c5b4575ef865408b6
Yizhou Chen <geoyee@yeah.net>
2022-09-18 14:35:28 +0800

    Update leaflet_china_correction.js

--- leaflet_china_correction.js
+++ leaflet_china_correction.js
@@ -1,3 +1,17 @@
+L.CRS.Baidu = new L.Proj.CRS('EPSG:900913', '+proj=merc +a=6378206 +b=6356584.314245179 +lat_ts=0.0 +lon_0=0.0 +x_0=0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs', {
+    resolutions: function () {
+        var level = 19
+        var res = [];
+        res[0] = Math.pow(2, 18);
+        for (var i = 1; i < level; i++) {
+            res[i] = Math.pow(2, (18 - i))
+        }
+        return res;
+    }(),
+    origin: [0, 0],
+    bounds: L.bounds([20037508.342789244, 0], [0, 20037508.342789244])
+});
+
 L.CoordConver = function () {
     var pi = 3.1415926535897932384626;
     var a = 6378245.0;
@@ -56,7 +70,7 @@ L.GridLayer.include({
     _setZoomTransform: function (level, _center, zoom) {
         var center = _center;
         if (center != undefined && this.options) {
-            if (this.options['attribution'].includes('GeoQ')) {
+            if (this.options['cn_crs'] == 'gcj02') {
                 center = L.coordConver().WGS84ToGCJ02(_center.lng, _center.lat);
             }
         }
